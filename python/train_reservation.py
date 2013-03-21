f6b2f8b81eb435d30c5dbcb95cb2995508a1d1e6
Emily Bache <emily@bacheconsulting.com>
2013-03-21 19:28:43 +0100

    don't pass around train objects all over the place

--- python/train_reservation.py
+++ python/train_reservation.py
@@ -1,17 +1,23 @@
 
 class TicketOffice(object):
+	def __init__(self, trains=None):
+		self.trains = trains or {}
+	
+	def add_train(self, train_id, train):
+		self.trains[train_id] = train
+
     def make_reservation(self, reservation_request):
         # TODO: write this code!
         pass
 
 class ReservationRequest(object):
-    def __init__(self, train, seat_count):
-        self.train = train
+    def __init__(self, train_id, seat_count):
+        self.train_id = train_id
         self.seat_count = seat_count
 
 class Reservation(object):
-    def __init__(self, train, seats=None, booking_reference=None):
-        self.train = train
+    def __init__(self, train_id, seats=None, booking_reference=None):
+        self.train_id = train_id
         self.seats = seats or []
         self.booking_reference = booking_reference
 
