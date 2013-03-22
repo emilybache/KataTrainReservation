13dc2bc9af1791a6d72c90264baee3d05f937482
emilybache <emily@bacheconsulting.com>
2013-03-22 14:26:50 +0100

    Make the starting code simpler

--- python/train_reservation.py
+++ python/train_reservation.py
@@ -1,11 +1,5 @@
 
 class TicketOffice(object):
-    def __init__(self, trains=None):
-        self.trains = trains or {}
-    
-    def add_train(self, train_id, train):
-        self.trains[train_id] = train
-
     def make_reservation(self, reservation_request):
         # TODO: write this code!
         pass
@@ -20,10 +14,6 @@ class Reservation(object):
         self.train_id = train_id
         self.seats = seats or []
         self.booking_reference = booking_reference
-
-class Train(object):
-    def __init__(self, seats=None):
-        self.seats = seats or []
 
 class Seat(object):
     def __init__(self, carriage, seat_number):
