c133224ee0516af0e27e8918b42fc5d57df81b43
Emily Bache <emily@bacheconsulting.com>
2013-04-14 10:36:04 +0200

    Attempt to return this clojure version to a starting position

--- clojure/train/src/train/core.clj
+++ clojure/train/src/train/core.clj
@@ -2,14 +2,7 @@
   (:use [midje.sweet :only [unfinished]])
   (:require [clojure.set :as set]))
 
-(unfinished take-seats)
+(defn make-reservation [reservation_request]
+  "TODO: Write code here")
 
-
-
-
-(defn make-reservation [request fleet-snapshot]
-  (let [seats-found (take-seats (:seat-count request)
-                                (get fleet-snapshot (:train-id request)))]
-    {:fleet-snapshot ["fred"]
-     :reservation (assoc request :seats seats-found)}))
  
--- clojure/train/test/train/t_core.clj
+++ clojure/train/test/train/t_core.clj
@@ -3,10 +3,10 @@
   (:use train.core)
   (:require [clojure.set :as set]))
 
-(def sample-seat {:carriage "A", :number 3})
-(def sample-reservation-request {:train-id "City of New Orleans"
+(def sample-seat {:coach "A", :seat-number 3})
+(def sample-reservation-request {:train-id "local_1000"
                                  :seat-count 1})
-(def sample-reservation-showing-only-required-fields {:train-id "City of New Orleans"
+(def sample-reservation-showing-only-required-fields {:train-id "local_1000"
                                                       :seats [sample-seat]
                                                       :booking-reference "387829"})
 
@@ -15,10 +15,5 @@
                                :booking-reference "387829"))
 
 
- 
-(fact "you can reserve a single seat"
-  (prerequisite (take-seats 1 [..1.. ..2..]) => [..2..])
-  (let [result (make-reservation {:train-id ..id.. :seat-count 1}
-                                 {..id.. [..1.. ..2..]})]
-    (:fleet-snapshot result) => {..id.. [..1..]}
-    (:reservation result) => (contains {:train-id ..id.., :seats [..2..]})))
+(fact "you can reserve seats"
+  "TODO: Write code here")
