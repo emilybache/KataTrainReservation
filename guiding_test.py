9f41fe505658a37edaa6e947bc437d6414503203
Emily Bache <emily@bacheconsulting.com>
2013-08-13 20:25:08 +0200

    make python starting code smaller

--- guiding_test.py
+++ guiding_test.py
@@ -23,22 +23,24 @@ reservation_script = os.path.join("python", "reserve.py")
 class TrainReservationTest(unittest.TestCase):
 
     def test_reserve_seats_via_POST(self):
-        form_data = {"train_id": "express_2000", "seats": 4}
+        form_data = {"train_id": "express_2000", "seat_count": 4}
         data = urllib.parse.urlencode(form_data)
         
         req = urllib.request.Request(url + "/reserve", bytes(data, encoding="ISO-8859-1"))
         response = urllib.request.urlopen(req).read().decode("ISO-8859-1")
         reservation = json.loads(response)
         
+        assert "express_2000" == reservation["train_id"]
         assert 4 == len(reservation["seats"])
         assert "1A" == reservation["seats"][0]
         assert "75bcd15" == reservation["booking_reference"]
 
 
-    def test_reserve_seats_via_cmd(self):
+    def hid_test_reserve_seats_via_cmd(self):
         response = subprocess.check_output([interpreter, reservation_script, "express2000", "4"], stderr=subprocess.STDOUT, universal_newlines = True)
         reservation = json.loads(response)
         
+        assert "express_2000" == reservation["train_id"]
         assert 4 == len(reservation["seats"])
         assert "1A" == reservation["seats"][0]
         assert "75bcd15" == reservation["booking_reference"]
--- python/ticket_office.py
+++ python/ticket_office.py
@@ -1,12 +1,13 @@
 
-from reservations import *
-
 class TicketOffice(object):
 
-    def __init__(self, train_data_service, booking_reference_service):
-        pass
-
-    def make_reservation(self, reservation_request):
+    def reserve(self, train_id, seat_count):
         # TODO: write this code!
         pass
 
+if __name__ == "__main__":
+    """Deploy this class as a web service using CherryPy"""
+    import cherrypy
+    TicketOffice.reserve.exposed = True
+    cherrypy.config.update({"server.socket_port" : 8083})
+    cherrypy.quickstart(TicketOffice())
