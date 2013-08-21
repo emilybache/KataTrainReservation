77a5ad39df6fa4706753d093a324c26a6cddf1fb
emilybache <emily@bacheconsulting.com>
2013-08-21 13:07:15 +0200

    ruby version should use guiding test instead of this

--- ruby/test_ticket_office.rb
+++ ruby/test_ticket_office.rb
@@ -5,14 +5,7 @@ require 'test/unit'
 class TestTicketOffice < Test::Unit::TestCase
 
   def test_reserve_seats
-    office = TicketOffice.new("http://localhost:8081", "http://localhost:8082")
-    request = ReservationRequest.new("express_2000", 4)
-    
-    reservation = office.make_reservation(request)
-    
-    assert_equal 4, reservation.seats.size()
-    assert_equal "A", reservation.seats[0].coach
-    assert_equal "75bcd15", reservation.booking_reference
+    # TODO: write this code!
   end
 
 end
