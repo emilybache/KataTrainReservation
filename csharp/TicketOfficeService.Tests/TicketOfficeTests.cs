08c338e290e1f670208dcc3648df337ebc2c6a61
Daan Stolp <mail@daanstolp.nl>
2022-02-19 14:22:19 +0100

    Add empty TicketOfficeService
    This is the service that needs to be implemented in the kata. An empty xUnit test project is included.

---
+++ csharp/TicketOfficeService.Tests/TicketOfficeTests.cs
@@ -0,0 +1,11 @@
+using Xunit;
+
+namespace TicketOfficeService.Tests;
+
+public class TicketOfficeTests
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ csharp/TicketOfficeService/TicketOffice.cs
@@ -0,0 +1,14 @@
+﻿namespace TicketOfficeService;
+
+public record ReservationRequest(string TrainId, int SeatCount);
+public record Reservation(string TrainId, string BookingId, List<Seat> Seats);
+public record Seat(string Coach, int SeatNumber);
+
+public class TicketOffice
+{
+    public Reservation MakeReservation(ReservationRequest request)
+    {
+        //TODO: implement this code!
+        throw new NotImplementedException();
+    }
+}
